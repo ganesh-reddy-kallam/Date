2627ee5d82fb59670c406c767c1fac116f622f0c
Naveen Reddy <nrkb1998@gmail.com>
2022-08-01 11:56:42 -0400

    adding intial files into the github

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Example
+{
+   public static void Main()
+   {
+      DateTime baseDate = new DateTime(2000, 2, 29);
+      Console.WriteLine("    Base Date:        {0:d}\n", baseDate);
+
+      // Show dates of previous fifteen years.
+      for (int ctr = -1; ctr >= -15; ctr--)
+         Console.WriteLine("{0,2} year(s) ago:        {1:d}",
+                           Math.Abs(ctr), baseDate.AddYears(ctr));
+      Console.WriteLine();
+
+      // Show dates of next fifteen years.
+      for (int ctr = 1; ctr <= 15; ctr++)
+         Console.WriteLine("{0,2} year(s) from now:   {1:d}",
+                           ctr, baseDate.AddYears(ctr));
+   }
+}
